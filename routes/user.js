7628a23f6a489db0555e98bef6629811db0689a6
Maiss <maissswanous@gmail.com>
2024-09-20 16:31:14 +0300

    checkCode

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,42 @@
 const express = require('express');
+const app = express()
+const session = require('express-session')
+const store = new session.MemoryStore()
+app.use(session({
+    secret: 'secter',
+    cookie:{maxAge:3000},
+    saveUninitialized: false,
+    store: store
+}));
 const router = express.Router();
 const userService = require("../service/userService");
-
+let checkCode = 0;
 router.post('/signup', async (req, res) => {
     try {
         const { username, phone, email, password } = req.body;
-        const newUser = await userService.createUser({ username, phone, email, password });
-        res.status(201).json({ message: 'User created successfully', user: newUser });
+        const userData = await userService.checkUser({ username, phone, email, password });
+        checkCode = parseInt(userData.checkCode);
+        req.session.userData = userData;
+        console.log(res.session.userData)
+
+        console.log(checkCode)
+        res.status(201).json({ message: 'The entered data is valid' });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Failed to create user' });
+        res.status(500).json({ error: 'Invalid data' });
     }
 });
+router.post('/checkCode', async (req, res) => {
+    try {
+        const userCode = parseInt(req.body.checkCode);
+        if (userCode == checkCode) {
+            userService.createUser();
+        } else {
+            console.log("errr")
+        }
+        res.send("")
+    }
+    catch (err) { console.log(err) }
+})
 
 module.exports = router;
--- service/userService.js
+++ service/userService.js
@@ -1,71 +1,70 @@
 const { user } = require('../models');
 const bcrypt = require('bcryptjs');
-
+const nodemailer = require('nodemailer')
 const userService = {
 
-    async checkEmail(email) {
-        // Validate email format using a regular expression
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        
-        if (!emailRegex.test(email.toLowerCase())) {
-            throw new Error('Invalid email format. Please enter a valid email address.');
-        }
-        // Check for existing user with the same email 
-        const existingUser = await user.findOne({ where: { email } });
-        if (existingUser) {
-            throw new Error('Email address already in use. Please try a different email.');
-        }
+  async checkEmail(email) {
+    // Validate email format using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        return true;
-    },
-     
-    async sendCode(email){
-
-        try {
-            const confirmCode = Math.floor(1000 + Math.random() * 9000).toString(); // Generate a random 4-digit number
-            const transporter = nodemailer.createTransport({
-              host: "smtp.elasticemail.com",
-              port: 2525,
-              auth: {
-                user: "hananalrstom87@gmail.com",
-                pass: "1980E59A59ABF2E83538525EF3B1FD9C1824",
-              },
-            });
-    
-            const info = await transporter.sendMail({
-              from: "hananalrstom87@gmail.com",
-              to: email,
-              subject: "confirm your email ",
-              text: "To confirm your password, please use the following One Time code:",
-              html: `To confirm, please use the following One Time code <strong>${confirmCode}</strong>`,
-            });
-    
-            console.log("Message sent:", info.messageId);
-          } catch (error) {
-            console.error("Error sending email:", error);
-          }
-
-    },
-
-    async createUser(userData) {
-        if (!userData.username || !userData.phone || !userData.email || !userData.password) {
-            throw new Error('Missing required fields');
-        }
-
-        // password encryption
-        const hashedPassword = await bcrypt.hash(userData.password, 10);
-        userData.password = hashedPassword;
-        userData.phone = parseInt(userData.phone)
-        try {
-            await this.checkEmail(userData.email); // Validate email and throw error if invalid
-            const newUser = await user.create(userData);
-            await this.sendCode(userData.email);
-            return newUser;
-        } catch (error) {
-            throw error;
-        }
-        
+    if (!emailRegex.test(email.toLowerCase())) {
+      throw new Error('Invalid email format. Please enter a valid email address.');
     }
+    // Check for existing user with the same email 
+    const existingUser = await user.findOne({ where: { email } });
+    if (existingUser) {
+      throw new Error('Email address already in use. Please try a different email.');
+    }
+
+    return true;
+  },
+
+  async sendCode(email) {
+
+    const confirmCode = Math.floor(1000 + Math.random() * 9000).toString(); // Generate a random 4-digit number
+    try {
+      const transporter = nodemailer.createTransport({
+        host: "smtp.elasticemail.com",
+        port: 2525,
+        auth: {
+          user: "whiteocjd@gmail.com",
+          pass: "70028532E494746FB9CD5CDA7519A033E123",
+        },
+      });
+
+      const info = await transporter.sendMail({
+        from: "whiteocjd@gmail.com",
+        to: email,
+        subject: "confirm your email ",
+        text: "To confirm your password, please use the following One Time code:",
+        html: `To confirm, please use the following One Time code <strong>${confirmCode}</strong>`,
+      });
+      console.log("Message sent:", info.messageId);
+    } catch (error) {
+      console.error("Error sending email:", error);
+    }
+    return confirmCode;
+  },
+  async checkUser(userData) {
+    if (!userData.username || !userData.phone || !userData.email || !userData.password) {
+      throw new Error('Missing required fields');
+    }
+
+    // password encryption
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    userData.password = hashedPassword;
+    userData.phone = parseInt(userData.phone)
+
+    try {
+     // await this.checkEmail(userData.email); // Validate email and throw error if invalid
+      const checkCode = await this.sendCode(userData.email);
+      return { checkCode, userData };
+    } catch (error) {
+      throw error;
+    }
+
+  },
+ 
 };
 
 module.exports = userService;
