88f09ad990ab9f6b27a449a3b056a4248050eff0
HananRstom <hananalrstom87@gmail.com>
2024-09-22 01:25:52 +0300

    post to post

--- view/src/components/Header.js
+++ view/src/components/Header.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import { Link } from "react-router-dom"
 import logo from "../img/logo.png"
 export default function Header() {
     return (<div className="container">
-        <nav className="d-flex">
+        <nav 
+        className="d-flex">
             <img src={logo} alt="لوغو الموقع" className="logo" />
             <div className="d-flex">
                 <Link to="/SignUp" style={{ textAlign: "center" }} className="register-nav" >انشاء حساب </Link>
