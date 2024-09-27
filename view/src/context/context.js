65c1d188694f1d692422d831aedd2a357e48ecb3
Maiss <maissswanous@gmail.com>
2024-09-27 17:15:29 +0300

    style

--- view/src/context/context.js
+++ view/src/context/context.js
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 
 export const User = createContext(null)
-export default function UserProvider(chiled) {
+export default function UserProvider() {
     const [auth, setAuth] = useState("")
-    return <User.Provider value={{ auth, setAuth }}>{chiled}</User.Provider>
+    return <User.Provider value={{ auth, setAuth }}></User.Provider>
 }
