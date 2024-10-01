d611c9298ce7ffee036e32d85c59daae16117144
Maiss <maissswanous@gmail.com>
2024-10-01 11:34:08 +0300

    forgin key sales

--- models/sales.js
+++ models/sales.js
@@ -22,7 +22,8 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.DATE
         }
     });
-
+    Product.hasMany(sales)
+    sales.belongsTo(Product)
     return sales;
 
 }
